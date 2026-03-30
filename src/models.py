ad64324c4d839fe11221ba52a3194e7c2301f6a9
Egor <egor.pyuretskiy@gmail.com>
2026-03-30 18:41:19 +0300

    Fix model field types

--- src/models.py
+++ src/models.py
@@ -4,25 +4,25 @@ db = SQLAlchemy()
 
 class Consignor(db.Model):
     id = db.Column(db.Integer, primary_key=True) # Уникальный идентификатор комитента
-    first_name = db.Column(db.varchar(50), nullable=False) # Имя комитента
-    last_name = db.Column(db.varchar(50), nullable=False) # Фамилия комитента
-    middle_name = db.Column(db.varchar(50), nullable=True) # Отчество комитента
-    email = db.Column(db.varchar(100), nullable=False) # Email комитента
-    phone_number = db.Column(db.varchar(20), nullable=False)  # Номер телефона комитента
-    passport_data = db.Column(db.varchar(50), nullable=False) # Паспортные данные комитента
-    INN = db.Column(db.varchar(50), nullable=False) # ИНН комитента
+    first_name = db.Column(db.String(50), nullable=False) # Имя комитента
+    last_name = db.Column(db.String(50), nullable=False) # Фамилия комитента
+    middle_name = db.Column(db.String(50), nullable=True) # Отчество комитента
+    email = db.Column(db.String(100), nullable=False) # Email комитента
+    phone_number = db.Column(db.String(20), nullable=False)  # Номер телефона комитента
+    passport_data = db.Column(db.String(50), nullable=False) # Паспортные данные комитента
+    INN = db.Column(db.String(50), nullable=False) # ИНН комитента
     sale_id = db.Column(db.Integer, db.ForeignKey('sale.id')) # Идентификатор продажи
     report_id = db.Column(db.Integer, db.ForeignKey('report.id')) # Идентификатор отчёта
 
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True) # Уникальный идентификатор товара
-    product_name = db.Column(db.varchar(100), nullable=False) # Наименование товара
-    description = db.Column(db.varchar(500), nullable=False) # Описание товара
-    delivery_date = db.Column(db.date, nullable=False) # Дата доставки товара
-    expiry_date = db.Column(db.date, nullable=False) # Срок реализации товара
-    price = db.Column(db.float, nullable=False) # Цена товара
-    status = db.Column(db.varchar(50), nullable=False) # Статус товара
+    product_name = db.Column(db.String(100), nullable=False) # Наименование товара
+    description = db.Column(db.String(500), nullable=False) # Описание товара
+    delivery_date = db.Column(db.Date, nullable=False) # Дата доставки товара
+    expiry_date = db.Column(db.Date, nullable=False) # Срок реализации товара
+    price = db.Column(db.Float, nullable=False) # Цена товара
+    status = db.Column(db.String(50), nullable=False) # Статус товара
     consignor_id = db.Column(db.Integer, db.ForeignKey('consignor.id')) # Идентификатор комитента
     sale_id = db.Column(db.Integer, db.ForeignKey('sale.id')) # Идентификатор продажи
     report_id = db.Column(db.Integer, db.ForeignKey('report.id')) # Идентификатор отчёта
@@ -30,16 +30,16 @@ class Product(db.Model):
 
 class Sale(db.Model):
     id = db.Column(db.Integer, primary_key=True) # Уникальный идентификатор продажи
-    sale_date = db.Column(db.date, nullable=False) # Дата продажи
-    sale_price = db.Column(db.float, nullable=False) # Цена продажи
-    commission = db.Column(db.float, nullable=False) # Комиссия
-    status = db.Column(db.varchar(50), nullable=False) # Статус
+    sale_date = db.Column(db.Date, nullable=False) # Дата продажи
+    sale_price = db.Column(db.Float, nullable=False) # Цена продажи
+    commission = db.Column(db.Float, nullable=False) # Комиссия
+    status = db.Column(db.String(50), nullable=False) # Статус
     consignor_id = db.Column(db.Integer, db.ForeignKey('consignor.id')) # Идентификатор комитента
 
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)  # Уникальный идентификатор отчета
-    number = db.Column(db.varchar(50), nullable=False)  # Номер отчета
+    number = db.Column(db.String(50), nullable=False)  # Номер отчета
     date = db.Column(db.Date, nullable=False)  # Дата отчета
-    report_type = db.Column(db.varchar(50), nullable=False)  # Тип отчета
-    description = db.Column(db.varchar(200), nullable=False)  # Описание отчета
+    report_type = db.Column(db.String(50), nullable=False)  # Тип отчета
+    description = db.Column(db.String(200), nullable=False)  # Описание отчета
